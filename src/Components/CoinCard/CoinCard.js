4f98557b4791c1a1cbd81f0cebb00e4e7537e146
nahiduzzaman2021 <akash.nahid.27@gmail.com>
2022-03-31 17:09:59 +0600

    coins

---
+++ src/Components/CoinCard/CoinCard.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './CoinCard.css'
+
+const CoinCard = ({ coin }) => {
+    return (
+        <div className='single-card'>
+            <Link to='/coins' className='coin-details'>
+
+                <div className='coin-info p-4 d-flex justify-content-between align-items-center'>
+                    <div className='coin-image'>
+                        <img src={coin.image} alt="coinImg" />
+                    </div>
+                    <div className='d-flex flex-column justify-content-end align-items-end'>
+                        <span className='coin-name'>{coin.name}</span>
+                        <span className='coin-symbol'>{coin.symbol}</span>
+
+                    </div>
+                </div>
+
+            </Link>
+        </div>
+    );
+};
+
+export default CoinCard;
--- src/Components/Coins/Coins.js
+++ src/Components/Coins/Coins.js
@@ -1,9 +1,32 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import CoinCard from '../CoinCard/CoinCard';
+import './Coins.css'
 
 const Coins = () => {
+    const [coins, setCoins] = useState([]);
+
+    useEffect(() => {
+        fetch('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=50&page=1&sparkline=false')
+            .then(res => res.json())
+            .then(data => setCoins(data))
+    }, []);
+
     return (
-        <div>
-            <h3>coins page</h3>
+        <div className='container-fluid'>
+            <div className='container mt-3'>
+                <h5 className='text-center'>Available Crypto Currencies</h5>
+                <p className='text-center'>Total Coins: {coins.length}</p>
+                <div className='row'>
+                    <div className='coins-container mt-5'>
+                        {
+                            coins.map(coin => <CoinCard
+                                key={coin.id}
+                                coin={coin}
+                            ></CoinCard>)
+                        }
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
